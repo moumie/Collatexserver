2ed5bf872744d9d904c7324300bf0cf6b8764567
soulemane <moumie.soulemane@gmail.com>
2016-03-14 12:25:10 +0100

    number of users online

--- server/server.js
+++ server/server.js
@@ -1,16 +1,23 @@
 var app = require('express')();
 var http = require('http').createServer(app)
 var io = require('socket.io').listen(http);
-
+var userOnline =0;
 
 //Server accepting connect on this port
 http.listen(3000);
 
 //Establishing connection to client and disconnecting
 io.sockets.on('connection', function(socket){
-    console.log('Connected to a new client')
+    console.log('Connected to a new client');
+    //Increment the number of user online
+    userOnline++;
+    socket.emit('user_online',userOnline);
+
     socket.on('disconnect', function(){
-    console.log('Disconnected from a client')
+    //Decrement the number of user online
+    userOnline--;
+    socket.emit('user_online',userOnline);
+    console.log('Disconnected from a client');
     });
     
     //Data exchange between client and server
